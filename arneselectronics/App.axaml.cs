f22954bd47f33c2317b0526d4d51b1dd3fa781c7
Niklas-Hakmann <nikha22@student.sdu.dk>
2025-02-17 12:09:05 +0100

    Forsøg på dynamisk billede integration

--- arneselectronics/App.axaml.cs
+++ arneselectronics/App.axaml.cs
@@ -1,14 +1,24 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using System;
+using Avalonia.Controls;
+using Avalonia.Platform;
 
 namespace arneselectronics;
 
 public partial class App : Application
 {
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        this.Loaded += (_, _) =>
+        {
+            Console.WriteLine("Window Loaded!");
+            LoadLogo();
+        };
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -19,5 +29,15 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+    private void LoadLogo()
+    {
+        var logoImage = this.GetControl<Image>("LogoImage");
+
+        var uri = new Uri("avares://arneselectronics/Assets/testbillede.jpg");
+        Console.WriteLine($"URI: {uri}");
+    
+        var bitmap = new Bitmap(AssetLoader.Open(uri));
+        logoImage.Source = bitmap;
     }
 }
