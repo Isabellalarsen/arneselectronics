3ec915e6be9793aa26588db0c40dc8a754b5e470
Niklas-Hakmann <nikha22@student.sdu.dk>
2025-02-23 12:36:12 +0100

    Ændring

--- arneselectronics/MainWindow.axaml.cs
+++ arneselectronics/MainWindow.axaml.cs
@@ -15,16 +15,15 @@ public partial class MainWindow : Window
         //Objects/Instance of products
         Products product1 = new Products("Lenovo Tablet", "Den sejeste tablet", 1234567910110, 1, 699.420);
         Products product2 = new Products("MacBook Pro", "Apple Laptop", 1234567910111, 1, 1299.99);
-        Product1NameTextBlock.Text = product1.Display("FrontPage1");
-        
-        Product2NameTextBlock.DataContext = product2;
+        Product1Block.Text = product1.Display("FrontPage1");
+        Product2Block.Text = product2.Display("FrontPage2");
        // ProductNameTextBlock.Bind(TextBlock.TextProperty, new Binding("productName"));
         
 
         // Directly load the logos when the window is initialized
         this.Loaded += (_, _) =>
         {
-            Console.WriteLine("Window Loaded!");
+            Console.WriteLine("Window Loaded! .");
             //Insert methods here instead to update cart counter and total Counter
             Total.Text += " 1";
             Quantity.Text += " 1";
