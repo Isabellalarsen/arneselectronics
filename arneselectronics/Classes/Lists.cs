a2b6349de905e31f4ff33da4cf1e773de4df890c
niklas-hakmann <nikha22@student.sdu.dk>
2025-03-20 14:06:52 +0100

    Singleton is now working, but needs to be loaded from every window. Not optimal?

--- arneselectronics/Classes/Lists.cs
+++ arneselectronics/Classes/Lists.cs
@@ -1,32 +1,43 @@
+using Avalonia.Controls;
+using System;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Collections;
+using Dapper;
+using Npgsql;
+
 
 namespace arneselectronics;
 
-public class ListInizializer
+public class ListInitializer
 {
-    
-    // Created new ObservableCollection of the type Products, added a get property to get collection content and instantiated the collection
-    // Lists are empty until filled by database when program is run
-    public ObservableCollection<Products> ProductsList { get; } = new ObservableCollection<Products>
-    {
-    };
-    
-    public ObservableCollection<Products> DesktopList { get; } = new ObservableCollection<Products> 
-    {
-    };
+    public static ListInitializer Instance { get; } = new ListInitializer();
 
-    public ObservableCollection<Products> LaptopList { get; } = new ObservableCollection<Products>
-    {
-    };
+    public ObservableCollection<Products> ProductsList { get; } = new();
+    public ObservableCollection<Products> DesktopList { get; } = new();
+    public ObservableCollection<Products> LaptopList { get; } = new();
+    public ObservableCollection<Products> HardwareList { get; } = new();
+    public ObservableCollection<Products> AccessoriesList { get; } = new();
 
-    public ObservableCollection<Products> HardwareList { get; } = new ObservableCollection<Products>
-    {
-    };
+    private ListInitializer() { }
 
-    public ObservableCollection<Products> AccessoriesList { get; } = new ObservableCollection<Products>
+    public void LoadProductsFromDatabase()
     {
-    };
-    
-    
-    
+        string conString = "Host=localhost;Username=postgres;Password=2231Niklas;Database=arne";
+
+        using (var con = new NpgsqlConnection(conString))
+        {
+            var data = con.Query<Products>("SELECT * FROM products").ToList();
+            foreach (var product in data)
+            {
+                ProductsList.Add(product);
+                if (product.Type == "desktop") DesktopList.Add(product);
+                else if (product.Type == "laptop") LaptopList.Add(product);
+                else if (product.Type == "hardware") HardwareList.Add(product);
+                else if (product.Type == "accessories") AccessoriesList.Add(product);
+            }
+        }
+    }
 }
+
--- arneselectronics/Views/StationærView.axaml.cs
+++ arneselectronics/Views/StationærView.axaml.cs
@@ -12,33 +12,8 @@ public partial class StationærView : UserControl
     public StationærView()
     {
         InitializeComponent();
-        ListInizializer listInstance = new ListInizializer();
+        ListInitializer listInstance = ListInitializer.Instance;
         DesktopListView.ItemsSource = listInstance.DesktopList;
-        string conString = "Host=localhost;Username=postgres;Password=Aebea2602$;Database=arneselectronics";
-        
-        using (var con = new NpgsqlConnection(conString))                                             
-        {                                                                                             
-            var data = con.Query<Products>("SELECT * FROM products").ToList();                        
-            foreach (var product in data)                                                             
-            {                                                                                         
-                listInstance.ProductsList.Add(product);
-                if (product.Type == "desktop")
-                {
-                    listInstance.DesktopList.Add(product);
-                }
-                else if (product.Type == "laptop")
-                {
-                    listInstance.LaptopList.Add(product);
-                }
-                else if (product.Type == "hardware")        
-                {                                         
-                    listInstance.HardwareList.Add(product); 
-                }
-                else if (product.Type == "accessories")        
-                {                                         
-                    listInstance.AccessoriesList.Add(product); 
-                }                                         
-            }
-        } 
+        listInstance.LoadProductsFromDatabase();
     }
 }
