b743824d637423199be40ca9887a1d16c59e1f6a
AndreasLC <andreaslykkecaspers@gmail.com>
2025-03-20 12:38:27 +0100

    Tab buttons are working, but we need to make the code better. (Singleton?)

--- arneselectronics/Views/MainView.axaml.cs
+++ arneselectronics/Views/MainView.axaml.cs
@@ -15,56 +15,19 @@ public partial class MainView : Window
     {
         InitializeComponent();
         DataContext = this;
-        
         //Instantiate lists to access from desktop from instance 
-        ListInizializer listInstance = new ListInizializer();
         //Links listInstance with ItemSource
-        ProductsListView.ItemsSource = listInstance.ProductsList;
-        DesktopListView.ItemsSource = listInstance.DesktopList;
-        LaptopListView.ItemsSource = listInstance.LaptopList;
-        HardwareListView.ItemsSource = listInstance.HardwareList;
-        AccessoriesListView.ItemsSource = listInstance.AccessoriesList;
-        
         // Directly load the logos when the window is initialized
         this.Loaded += (_, _) =>
         {
             // Remember to add Resources to Avalonia 
             var ImageHandler = new ImageHandler();
             Console.WriteLine("Window Loaded!");
-
             //Insert methods here instead to update cart counter and total Counter
             Total.Text += " 1";
             Quantity.Text += " 1";
             ImageHandler.LoadImage(this, "LogoImage", "avares://arneselectronics/Assets/testbillede1.jpg");
             ImageHandler.LoadImage(this, "CartLogo", "avares://arneselectronics/Assets/carticon.jpg");
         };
-       
-        string conString = "Host=localhost;Username=postgres;Password=Aebea2602$;Database=arneselectronics";
-        
-        using (var con = new NpgsqlConnection(conString))                                             
-        {                                                                                             
-            var data = con.Query<Products>("SELECT * FROM products").ToList();                        
-            foreach (var product in data)                                                             
-            {                                                                                         
-                listInstance.ProductsList.Add(product);
-                if (product.Type == "desktop")
-                {
-                    listInstance.DesktopList.Add(product);
-                }
-                else if (product.Type == "laptop")
-                {
-                    listInstance.LaptopList.Add(product);
-                }
-                else if (product.Type == "hardware")        
-                {                                         
-                    listInstance.HardwareList.Add(product); 
-                }
-                else if (product.Type == "accessories")        
-                {                                         
-                    listInstance.AccessoriesList.Add(product); 
-                }                                         
-            }
-        } 
-    }
+    }  
 }
-    
