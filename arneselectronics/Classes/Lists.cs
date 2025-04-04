7149bc26c6f9ea7ddf82400d615273ea1d88d6a1
AndreasLC <andreaslykkecaspers@gmail.com>
2025-04-04 10:04:36 +0200

    Credentials.....

--- arneselectronics/Classes/Lists.cs
+++ arneselectronics/Classes/Lists.cs
@@ -1,7 +1,6 @@
 using System.Collections.ObjectModel;
 using System.Linq;
-using arneselectronics.Crendentials;
-using arneselectronics.Crendentials;
+using arneselectronics.Credentials;
 using Dapper;
 using Npgsql;
 
@@ -27,7 +26,7 @@ public class ListInitializer
 
     public void LoadProductsFromDatabase()
     {
-        Credentials credentials = new();
+        Credentials.Credentials credentials = new();
         string conString = credentials.Database;
 
         using (var con = new NpgsqlConnection(conString))
